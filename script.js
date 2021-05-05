f2995f95ed4f5a5f0b6441ce1c230c4598268166
Ana-Maria Demian <ana-mariademian@Ana-Marias-MacBook-Pro.local>
2021-05-05 12:42:44 +0300

    handlers are now set when the window is loaded

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-let toggle = false;
+window.onload = () => {
+    let toggle = false;
 
-let blackSquare = document.getElementById("black");
+    let blackSquare = document.getElementById("black");
 
-blackSquare.addEventListener('click', () => {
-    setTimeout( () => {
-        blackSquare.style.animation = 'move-black 10s linear';
-    }, 200);
+    blackSquare.addEventListener('click', () => {
+        setTimeout( () => {
+            blackSquare.style.animation = 'move-black 10s linear';
+        }, 200);
 
-});
+    });
 
-let greenSquare = document.getElementById("green");
-greenSquare.addEventListener('click', () => {
-    setTimeout(() => {
-        greenSquare.style.animation = 'move-green 10s linear';
-    }, 200);
-});
+    let greenSquare = document.getElementById("green");
+    greenSquare.addEventListener('click', () => {
+        setTimeout(() => {
+            greenSquare.style.animation = 'move-green 10s linear';
+        }, 200);
+    });
 
-let redSquare = document.getElementById("red");
-redSquare.addEventListener('click', () =>{
-    setTimeout(() => {
-        toggle = !toggle;
-        if(!toggle){
-            console.log()
-            redSquare.classList.remove('move-red-first');
-            redSquare.classList.add('move-red-second');
-        }else {
-            redSquare.classList.remove('move-red-second');
-            redSquare.classList.add('move-red-first');
-            // redSquare.style.animation = 'move-red 10s linear';
-            // redSquare.style.animationFillMode = 'forwards';
-        }
-    }, 200);
-})
+    let redSquare = document.getElementById("red");
+    redSquare.addEventListener('click', () =>{
+        setTimeout(() => {
+            toggle = !toggle;
+            if(!toggle){
+                console.log()
+                redSquare.classList.remove('move-red-first');
+                redSquare.classList.add('move-red-second');
+            }else {
+                redSquare.classList.remove('move-red-second');
+                redSquare.classList.add('move-red-first');
+                // redSquare.style.animation = 'move-red 10s linear';
+                // redSquare.style.animationFillMode = 'forwards';
+            }
+        }, 200);
+    })
+}
