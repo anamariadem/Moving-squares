2b37883e77385a3ee2c2d854dd5b71cc8097480f
Ana-Maria Demian <ana-mariademian@Ana-Marias-MacBook-Pro.local>
2021-05-05 13:02:08 +0300

    removed timeout

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ window.onload = () => {
     let blackSquare = document.getElementById("black");
 
     blackSquare.addEventListener('click', () => {
-        setTimeout( () => {
+        // setTimeout( () => {
             blackSquare.style.animation = 'move-black 10s linear';
-        }, 200);
+        // }, 200);
 
     });
 
     let greenSquare = document.getElementById("green");
     greenSquare.addEventListener('click', () => {
-        setTimeout(() => {
+        // setTimeout(() => {
             greenSquare.style.animation = 'move-green 10s linear';
-        }, 200);
+        // }, 200);
     });
 
     let redSquare = document.getElementById("red");
     redSquare.addEventListener('click', () =>{
-        setTimeout(() => {
+        // setTimeout(() => {
             toggle = !toggle;
             if(!toggle){
                 console.log()
@@ -31,6 +31,6 @@ window.onload = () => {
                 // redSquare.style.animation = 'move-red 10s linear';
                 // redSquare.style.animationFillMode = 'forwards';
             }
-        }, 200);
+        // }, 200);
     })
 }
